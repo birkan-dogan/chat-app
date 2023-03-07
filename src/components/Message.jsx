b4d19e62158e6f34a92a1a23dec04eb3197616e4
birkan <b.561birkan@gmail.com>
2023-03-07 17:30:31 +0300

    working on Message component styling 🎨

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,23 @@
 const Message = function () {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      {/* not functional yet */}
+      <div className="message-info">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+          alt=""
+        />
+        <span>Just now</span>
+      </div>
+      <div className="message-content">
+        <p>hello</p>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
---
+++ src/components/Messages.jsx
@@ -0,0 +1,18 @@
+import Message from "./Message";
+
+const Messages = function () {
+  return (
+    <div className="messages">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
