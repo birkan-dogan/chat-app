28b17f92828d85ca2db134ed0883a9570d6e631d
birkan <b.561birkan@gmail.com>
2023-03-07 17:09:42 +0300

    working on Chat component styling ✔

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,21 @@
 import Input from "./Input";
-import Message from "./Message";
+import Cam from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
+import Messages from "./Messages";
 
 const Chat = function () {
   return (
     <div className="chat">
-      <Message />
+      <div className="chatInfo">
+        <span>Hank</span> {/* username */}
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+      <Messages />
       <Input />
     </div>
   );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,23 @@
+import Img from "../images/img.png";
+import Attach from "../images/attach.png";
+
 const Input = function () {
-  return <div>Input</div>;
+  return (
+    <form className="input">
+      <input type="text" placeholder="Type something..." />
+      <div className="send">
+        <input type="file" style={{ display: "none" }} id="input-file" />
+        <label htmlFor="input-file">
+          <img src={Attach} alt="" />
+        </label>
+        <input type="file" style={{ display: "none" }} id="input-file" />
+        <label htmlFor="input-file">
+          <img src={Img} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </form>
+  );
 };
 
 export default Input;
