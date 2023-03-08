c2956d158bd0552f2a503f006af3125c1b52ff2d
birkan <b.561birkan@gmail.com>
2023-03-08 13:55:46 +0300

    Login process works 🔑

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   updateProfile,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 
 import {
@@ -28,7 +29,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication
-const auth = getAuth();
+export const auth = getAuth();
 
 // To upload a file to Cloud Storage
 // docs: https://firebase.google.com/docs/storage/web/upload-files#upload_files
@@ -40,7 +41,13 @@ const db = getFirestore();
 // register new user
 // docs: https://firebase.google.com/docs/auth/web/start?hl=en&authuser=0#sign_up_new_users
 
-export const signUp = async function (email, password, displayName, file) {
+export const signUp = async function (
+  email,
+  password,
+  displayName,
+  file,
+  navigate
+) {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const storageRef = ref(storage, displayName);
@@ -61,10 +68,30 @@ export const signUp = async function (email, password, displayName, file) {
             email,
             photoURL: downloadURL,
           });
+
+          await setDoc(doc(db, "userChats", res.user.uid), {}); // we need the userChats collection to use inside the chats
+
+          navigate("/");
         });
       }
     );
   } catch (error) {
     console.log(error);
   }
+};
+
+// Sign in a user with an email address and password
+// docs: https://firebase.google.com/docs/auth/web/password-auth#sign_in_a_user_with_an_email_address_and_password
+
+export const signIn = function (email, password, navigate) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,30 @@
+import { Link, useNavigate } from "react-router-dom";
+import { signIn } from "../firebase";
+
 const Login = function () {
+  const navigate = useNavigate();
+
+  const handleSubmit = async function (e) {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn(email, password, navigate);
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Chat App</span>
         <span className="title">Login</span>
-        <form>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" required />
+          <input type="password" placeholder="password" required />
           <button>Sign In</button>
         </form>
-        <p>Don't have an account: Register</p>
+        <p>
+          Don't have an account: <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
