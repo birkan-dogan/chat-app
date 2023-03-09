b3cce5fe4d2f731053cec16bec76e1d9484f150d
birkan <b.561birkan@gmail.com>
2023-03-09 13:38:18 +0300

    creating ChatContext to share chat information to relevant components ☁

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { useChatContext } from "../context/ChatContext";
 import { getChats } from "../firebase";
 
 const Chats = () => {
   const [chats, setChats] = useState([]);
+
   const { currentUser } = useAuthContext();
+  const { dispatch } = useChatContext();
 
   useEffect(() => {
     // to see realtime updates on firestore, we can use onSnapshot() method
 
     currentUser.uid && getChats(currentUser.uid, setChats);
   }, [currentUser.uid]);
+
+  const handleSelect = (user) => {
+    dispatch({ type: "Change_user", payload: user });
+  };
+
   return (
     <div className="chats">
-      {/* data will come from firestore */}
       {Object.entries(chats)?.map((chat) => (
-        <div className="userChat" key={chat[0]}>
+        <div
+          className="userChat"
+          key={chat[0]}
+          onClick={() => handleSelect(chat[1].userInfo)}
+        >
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
---
+++ src/context/ChatContext.js
@@ -0,0 +1,40 @@
+import { createContext, useContext, useReducer } from "react";
+import { useAuthContext } from "./AuthContext";
+
+const ChatContext = createContext();
+
+const ChatContextProvider = function ({ children }) {
+  const { currentUser } = useAuthContext();
+
+  const INITIAL_STATE = {
+    chatId: null,
+    user: {},
+  };
+
+  // creating reducer
+  const chatReducer = (state, action) => {
+    switch (action.type) {
+      case "Change_user":
+        return {
+          user: action.payload,
+          chatId: currentUser.uid + action.payload.uid,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE);
+
+  return (
+    <ChatContext.Provider value={{ data: state, dispatch }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = function () {
+  return useContext(ChatContext);
+};
+
+export default ChatContextProvider;
