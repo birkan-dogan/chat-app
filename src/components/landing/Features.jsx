0038a121d8fa186c860e06e9b582e7d31a0f7c88
birkan <b.561birkan@gmail.com>
2023-03-13 23:59:34 +0300

    looks sharp 🎉

--- src/components/landing/Features.jsx
+++ src/components/landing/Features.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import data from "../../helpers/featureData";
+import blob from "../../images/featuresBackground.svg";
 
 const Features = () => {
   return (
     <div className="feature">
       <div className="features-title">
-        <h1>Amazing features for to make your life easier</h1>
+        <h1>Amazing features to make your life easier</h1>
         <p>
           Send your messages to your friends instantly, stay connected and keep
           in touch with people who matter most.
@@ -19,6 +20,17 @@ const Features = () => {
             <p>{element.text}</p>
           </div>
         ))}
+        <img src={blob} alt="" className="blob right" />
+        <img src={blob} alt="" className="blob left" />
+      </div>
+      <div className="how-it-works">
+        <div className="works-title">
+          <h1>How it works?</h1>
+          <p>
+            The app is web-based, you don't have to install it to your device,
+            just create an account and find your friends to chat ✨
+          </p>
+        </div>
       </div>
     </div>
   );
