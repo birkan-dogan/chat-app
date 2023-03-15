7367c1d3863ba8523c5f7c264ff3263bda7056fe
birkan <b.561birkan@gmail.com>
2023-03-15 19:19:29 +0300

    working on Menu component ✔

--- src/components/landing/Menu.jsx
+++ src/components/landing/Menu.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import close from "../../images/close.svg";
+
 const Menu = ({ setResponsive }) => {
   return (
-    <div className="menu">
+    <div className="menu" data-aos="fade-down" data-aos-duration="500">
       <nav>
         <ul onClick={() => setResponsive(false)}>
-          <a>Features</a>
-          <a>About</a>
-          <a>How It Works</a>
-          <a>Support</a>
+          <a href="#features">Features</a>
+          <a href="#about">About</a>
+          <a href="#works">How It Works</a>
+          <a href="#support">Support</a>
         </ul>
       </nav>
       <div className="buttons-div">
